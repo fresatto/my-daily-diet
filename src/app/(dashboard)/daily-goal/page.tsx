f1528b10dc838f66bd114c8d2e4bd345c3bd7b20
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-29 20:31:17 -0300

    feat: listando o objetivo diário

--- src/app/(dashboard)/daily-goal/page.tsx
+++ src/app/(dashboard)/daily-goal/page.tsx
@@ -1,13 +1,41 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { api } from "@/services/api";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
 export default function DailyGoal() {
+  const { data } = useQuery({
+    queryKey: ["daily-goal"],
+    queryFn: async () => {
+      const response = await api.get<any>("/daily-goal");
+
+      return response.data;
+    },
+  });
+
+  const { data: summary } = useQuery({
+    queryKey: ["daily-goal-summary"],
+    queryFn: async () => {
+      const response = await api.get<any>("/daily-goal/summary");
+
+      return response.data;
+    },
+  });
+
+  const proteinConsumed = summary?.proteinConsumed || 0;
+  const proteinGoal = data?.dailyGoal?.protein || 0;
+  const proteinDifference = Number(proteinGoal - proteinConsumed).toFixed(1);
+
   return (
     <div className="flex flex-col gap-4">
-      <h3 className="text-center text-2xl font-bold">128g / 170g</h3>
+      <h3 className="text-center text-2xl font-bold">
+        {proteinConsumed}g / {proteinGoal}g
+      </h3>
       <p className="text-center text-sm">
-        Você consumiu <strong>128g</strong> de proteínas hoje, você está à{" "}
-        <strong>20g</strong> do seu objetivo.
+        Você consumiu <strong>{summary?.proteinConsumed}g</strong> de proteínas
+        hoje, você está à <strong>{proteinDifference}g</strong> do seu objetivo.
       </p>
       <Button className="self-center" asChild>
         <Link href="/">Alterar objetivo</Link>
