13804cb253ec12e342dd173e4266ffecfc3a64ff
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-14 22:29:33 -0300

    feat: wrapping root layout with Suspense

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Suspense>{children}</Suspense>
+    </QueryClientProvider>
   );
 }
