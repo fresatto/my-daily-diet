0ee3bb791226e5ebe35b1bc63b816d9733c53e45
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-16 18:50:22 -0300

    feat: adição de novo layout no DashboardLoadingFallback com seção para exibir refeições de hoje

--- src/app/components/DashboardLoadingFallback/index.tsx
+++ src/app/components/DashboardLoadingFallback/index.tsx
@@ -6,27 +6,37 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const DashboardLoadingFallback: React.FC = () => {
   return (
-    <Card.Container>
-      <div className="flex items-center justify-between">
-        <h3 className="font-bold">Resumo</h3>
-        <Utensils />
-      </div>
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-[120px]" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-[120px]" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-[120px]" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-[120px]" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-    </Card.Container>
+    <>
+      <Card.Container>
+        <div className="flex items-center justify-between">
+          <h3 className="font-bold">Resumo</h3>
+          <Utensils />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-4 w-[120px]" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-4 w-[120px]" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-4 w-[120px]" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-4 w-[120px]" />
+          <Skeleton className="h-4 w-full" />
+        </div>
+      </Card.Container>
+      <Card.Container>
+        <div className="flex items-center gap-2">
+          <Utensils size={16} />
+          <h3 className="text-sm font-bold">Refeições de hoje</h3>
+        </div>
+        <Skeleton className="h-15 w-full" />
+        <Skeleton className="h-15 w-full" />
+      </Card.Container>
+    </>
   );
 };
