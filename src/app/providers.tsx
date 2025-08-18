839b7657b527e9ebf0ae0c4591fe0fc470491a8f
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-17 22:44:30 -0300

    refactor: utilização de useState para instanciar o QueryClient no componente Providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
