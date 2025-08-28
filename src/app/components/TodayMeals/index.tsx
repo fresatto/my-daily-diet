e47ad862537ba5b06c12d9bf346ab585f0773b40
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-27 21:17:35 -0300

    feat: listando os alimentos por refeição

--- src/app/components/TodayMeals/index.tsx
+++ src/app/components/TodayMeals/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Utensils } from "lucide-react";
+import { BicepsFlexed, Scale, Utensils } from "lucide-react";
 
 import { Card } from "@/components/Card";
 import { Button } from "@/components/ui/button";
@@ -35,24 +35,52 @@ export const TodayMeals = () => {
       );
     }
 
-    return data?.meals?.map((meal) => (
-      <div
-        data-testid="meal-card"
-        key={meal.id}
-        className="flex py-3 px-4 justify-between items-center rounded-lg bg-gray-100"
-      >
-        <div className="flex flex-col">
-          <h3 data-testid="meal-name" className="text-sm font-bold">
-            {meal.name}
-          </h3>
-          <small data-testid="meal-time">{meal.formattedTime}</small>
+    return data?.meals?.map((meal) => {
+      const totalProtein = Number(meal.total_protein);
+
+      return (
+        <div
+          data-testid="meal-card"
+          key={meal.id}
+          className="flex py-3 px-4 justify-between items-center rounded-lg bg-gray-100"
+        >
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex items-center justify-between">
+              <h3 data-testid="meal-name" className="text-sm font-bold">
+                {meal.name}
+              </h3>
+              <div className="flex items-center gap-1 text-xs">
+                <BicepsFlexed size={14} />
+                <span className="text-xs">{totalProtein}g</span>
+              </div>
+            </div>
+            <ul>
+              {meal.items.map((item) => (
+                <li key={item.food_id}>
+                  <div className="flex items-center gap-4">
+                    <span className="text-sm ">{item.food_name}</span>
+                    <div className="flex items-center gap-1">
+                      <Scale size={12} />
+                      <span className="text-xs">
+                        {item.amount}
+                        {item.portion_type === "grams" ? "g" : "u"}
+                      </span>
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <BicepsFlexed size={12} />
+                      <span className="text-xs">
+                        {item.calculated_protein}g
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <small data-testid="meal-time">{meal.formattedTime}</small>
+          </div>
         </div>
-        <div className="flex flex-col items-end text-xs">
-          <p className="font-bold">{meal.total_protein}g</p>
-          <p>proteína</p>
-        </div>
-      </div>
-    ));
+      );
+    });
   };
 
   return (
