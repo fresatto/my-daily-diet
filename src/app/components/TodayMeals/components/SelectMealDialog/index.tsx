da8d47a3aecf9b5e928ac3b31e7a5eaa6004ca9c
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-31 22:02:42 -0300

    feat: srp on select meal dialog

--- src/app/components/TodayMeals/components/SelectMealDialog/index.tsx
+++ src/app/components/TodayMeals/components/SelectMealDialog/index.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
 
 import { Card } from "@/components/Card";
@@ -16,32 +14,14 @@ import { useConsumedMealsMutation } from "@/services/queries/consumed-meals";
 import { useMealsQuery } from "@/services/queries/meals";
 import { useDialog } from "@/hooks/useDialog";
 import { MealList } from "@/components/MealList";
-
-type SelectMealDialogProps = {
-  children: React.ReactNode;
-};
+import { SelectMealDialogMealsListProps, SelectMealDialogProps } from "./types";
 
 export const SelectMealDialog = ({ children }: SelectMealDialogProps) => {
+  const { data: mealsData, error } = useMealsQuery();
+
   const { open, handleOpenChange } = useDialog();
-  const { data, error } = useMealsQuery();
-  const {
-    mutate: createConsumedMeal,
-    variables,
-    isPending,
-  } = useConsumedMealsMutation({
-    onSuccess: () => {
-      handleOpenChange(false);
-    },
-    onError: () => {
-      toast.error("Erro ao registrar.");
-    },
-  });
 
-  const isEmpty = data?.meals.length === 0;
-
-  const handleSelectMeal = (mealId: string) => {
-    createConsumedMeal({ meal_id: mealId });
-  };
+  const isEmpty = mealsData?.meals.length === 0;
 
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
@@ -57,36 +37,56 @@ export const SelectMealDialog = ({ children }: SelectMealDialogProps) => {
           </p>
         )}
         {!error && !isEmpty && (
-          <>
-            <p className="text-sm text-gray-500">
-              Selecione uma das <strong>refeições salvas</strong> para registrar
-              o consumo.
-            </p>
-
-            <div className="max-h-[400px] overflow-y-auto hide-scrollbar">
-              {data?.meals && (
-                <MealList.List
-                  meals={data.meals}
-                  renderMeal={(meal) => {
-                    const isLoading =
-                      variables?.meal_id === meal.id && isPending;
-
-                    return (
-                      <button
-                        key={meal.id}
-                        className="cursor-pointer  hover:brightness-90 transition-all"
-                        onClick={() => handleSelectMeal(meal.id)}
-                      >
-                        <MealItemList meal={meal} isLoading={isLoading} />
-                      </button>
-                    );
-                  }}
-                />
-              )}
-            </div>
-          </>
+          <SelectMealDialogMealsList
+            onSuccessCreateConsumedMeal={() => handleOpenChange(false)}
+          />
         )}
       </DialogContent>
     </Dialog>
   );
 };
+
+function SelectMealDialogMealsList({
+  onSuccessCreateConsumedMeal,
+}: SelectMealDialogMealsListProps) {
+  const { data: mealsData } = useMealsQuery();
+
+  const {
+    mutate: createConsumedMeal,
+    variables,
+    isPending,
+  } = useConsumedMealsMutation({
+    onSuccess: onSuccessCreateConsumedMeal,
+    onError: () => toast.error("Erro ao registrar."),
+  });
+
+  return (
+    <>
+      <p className="text-sm text-gray-500">
+        Selecione uma das <strong>refeições salvas</strong> para registrar o
+        consumo.
+      </p>
+
+      <div className="max-h-[400px] overflow-y-auto hide-scrollbar">
+        {mealsData?.meals && (
+          <MealList.List
+            meals={mealsData.meals}
+            renderMeal={(meal) => {
+              const isLoading = variables?.meal_id === meal.id && isPending;
+
+              return (
+                <button
+                  key={meal.id}
+                  className="cursor-pointer  hover:brightness-90 transition-all"
+                  onClick={() => createConsumedMeal({ meal_id: meal.id })}
+                >
+                  <MealItemList meal={meal} isLoading={isLoading} />
+                </button>
+              );
+            }}
+          />
+        )}
+      </div>
+    </>
+  );
+}
---
+++ src/app/components/TodayMeals/components/SelectMealDialog/types.ts
@@ -0,0 +1,7 @@
+export type SelectMealDialogProps = {
+  children: React.ReactNode;
+};
+
+export type SelectMealDialogMealsListProps = {
+  onSuccessCreateConsumedMeal: () => void;
+};
